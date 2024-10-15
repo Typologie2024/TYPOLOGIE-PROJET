3f042ca3a83510d04f2a23e34c4f77bb46607747
DVTPROJET <dvtgroupe@gmail.com>
2024-10-15 14:43:40 +0100

    Mise à jour de la clé API

--- map.js
+++ map.js
@@ -9,7 +9,7 @@ require([
     "esri/PopupTemplate"
   ], function(esriConfig, Map, MapView, FeatureLayer, Legend, Expand, Search, PopupTemplate) {
     
-    esriConfig.apiKey = "AAPT3NKHt6i2urmWtqOuugvr9eCsBZ6_O8t9W_sCdDNnfgULsaM392PrWWmN45f5jfdPeTdg7dzbYeeLTfOpaetGDwNogmUz8MgDc2Ux35PCltIphBDpMKVHWgzQxx45nSeR1MHV0oYKWrlNcDaJxxoaxn0m0dtmZMtnRKa6IHhvNLWhkcakntz_rXoeyfZ0vagVIytMHwTmIodThsnAMCws0RzWFaUfrSPNPSvb3dQGaGGaBcFwriNnuRMt26ZHMh4G";
+    esriConfig.apiKey = "AAPT3NKHt6i2urmWtqOuugvr9eCsBZ6_O8t9W_sCdDNnfgVjmObUBJmjqKZdCT_N4pbutpZIdRtoRcPg0z7CegXzW_Q66k5xqECqWvEkfeOXWcfuUwGZz91gOuZ7HwAg9JD09EXxZLAoZcZFS6QSoiwUT2dPmHMjn4zjShLQnJg342AoaZJ3Q9k-4Sb4Xj_vwvJWkPWKdKx1P62buDnxEL1A0JwDURo9mX3wRcl1UbN6k_BX59clx0WZ3wSYJAy5joRG";
 
     // Créer la carte avec un fond de carte satellite
     const map = new Map({
